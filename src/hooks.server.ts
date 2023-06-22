1367cd995250518796be122de1b82ba7400e924e
Ethan Steere <62357808+e253@users.noreply.github.com>
2023-06-22 16:21:38 -0400

    auth middleware start.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,3 +1,26 @@
 // Start up code to migrate database. Not the cleanest, but should get the job done.
 import { execSync } from 'child_process'
 execSync(`npx prisma migrate deploy --schema /aels/prisma/schema.prisma`, { stdio: "inherit" })
+
+
+// Auth Middleware
+import type { Handle } from '@sveltejs/kit'
+import { AuthStore } from '$lib/server/auth'
+
+
+export const handle = (async ({ event, resolve }) => {
+   event.locals.auth_store = new AuthStore()
+   event.locals.auth_store.load_from_cookie(event)
+
+   if (event.locals.auth_store.isValid) {
+      event.locals.auth_store.refresh()
+   } else {
+      event.locals.auth_store.clear()
+   }
+
+   const response = await resolve(event)
+
+   response.headers.append('Set-Cookie', event.locals.auth_store.export_to_cookie())
+
+   return response
+}) satisfies Handle
--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,32 +1,65 @@
 import type { RequestEvent } from '@sveltejs/kit'
+import { prisma } from '$lib/server/prisma-client'
 import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
 
 export class AuthStore {
    user_id: number | null = null
-   token: string | null = null
+   token: string = ''
    isValid: boolean = false
+   JWT_SECRET = process.env.JWT_SECRET || ''
 
    load_from_cookie(event: RequestEvent): void {
-      let cookie = event.cookies.get("JWT_TOKEN")
+      let cookie = event.cookies.get("Authorization")
       if (cookie) {
-         token = cookie.split(" ")[0]
-         try {
-            user_id = jwt.verify(token, process.env.JWT_SECRET).user_id
-            isValid = true
-         }
+         this.token = cookie.split(' ')[0] // Dump Bearer prefix
+         const decoded_token_body = jwt.verify(this.token, this.JWT_SECRET) as JWT_BODY
+         this.user_id = decoded_token_body.user_id
+         this.isValid = true
       }
    }
 
    refresh(): boolean {
-      if (token && user_id && isValid) {
-         token = jwt.sign({ user_id: user_id }, process.env.JWT_SECRET, { epxiresIn: "1d" })
+      if (this.token && this.user_id && this.isValid) {
+         this.token = jwt.sign({ user_id: this.user_id }, this.JWT_SECRET, { expiresIn: "1d" })
          return true
       }
       return false
    }
 
-   export_to_cookie(event: RequestEvent): void {
-      event.cookies.set("JWT_TOKEN", token)
+   export_to_cookie(): string {
+      return `Authorization=Bearer ${this.token}`
+   }
+
+   async hydrate_with_email_password(email: string, password: string): Promise<boolean> {
+      const user = await prisma.user.findUnique({
+         where: {
+            email
+         }
+      })
+      if (!user) {
+         return false
+      }
+
+      const valid = await bcrypt.compare(password, user.password)
+      if (!valid) {
+         return false
+      }
+
+      this.user_id = user.id
+      this.token = jwt.sign({ user_id: this.user_id }, this.JWT_SECRET, { expiresIn: "1d" })
+      this.isValid = true
+      return true
+   }
+
+   clear(): void {
+      this.user_id = null
+      this.token = ''
+      this.isValid = false
    }
 }
+
+type JWT_BODY = {
+   user_id: number
+} 
