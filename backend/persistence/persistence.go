f6227d317bd4d3dfb654878d922c939945885323
e253 <ethansteere1@gmail.com>
2023-08-16 02:57:24 -0400

    added fly toml. Turn on ssl in production

--- backend/persistence/persistence.go
+++ backend/persistence/persistence.go
@@ -17,18 +17,25 @@ func InitAndMigrate() {
 	PG_PASS := os.Getenv("PG_PASS")
 	PG_PORT := os.Getenv("PG_PORT")
 	PG_DB := os.Getenv("PG_DB")
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", PG_HOST, PG_USER, PG_PASS, PG_DB, PG_PORT)
+	// TODO Hacky fix for turning on ssl in demo-production
+	var SSL_MODE string
+	if PG_USER == "e253" {
+		SSL_MODE = "verify-full"
+	} else {
+		SSL_MODE = "disabled"
+	}
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", PG_HOST, PG_USER, PG_PASS, PG_DB, PG_PORT, SSL_MODE)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(fmt.Sprintf("Error connecting to pg: %s", err.Error()))
 	}
 
-	err = db.AutoMigrate(&User{})
-	err = db.AutoMigrate(&Chat{})
-	err = db.AutoMigrate(&Message{})
-	if err != nil {
-		panic("Error occured during migration\nPlease Contact Support")
+	uerr := db.AutoMigrate(&User{})
+	cerr := db.AutoMigrate(&Chat{})
+	merr := db.AutoMigrate(&Message{})
+	if uerr != nil || cerr != nil || merr != nil {
+		panic("Error occured during migration")
 	}
 
 	DB = db
